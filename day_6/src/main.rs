01acaac13e791e689574628255d8ef51cfc277ed
Roan Paulus <roan.paulus@amsterdam.com>
2024-12-31 16:43:46 +0100

    part one solved

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -2,10 +2,12 @@ use macroquad::prelude::*;
 use miniquad::window::order_quit;
 use std::fs::File;
 use std::io::Read;
+use std::thread::sleep;
+use std::time::Duration;
 
 type Grid = Vec<Vec<char>>;
 const GUARD_START: char = '^';
-const OBSTABLE: char = '#';
+const OBSTACLE: char = '#';
 
 fn window_conf() -> Conf {
     Conf {
@@ -19,77 +21,123 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    // TODO: small.txt gives one position to little (should be 41, gives 40).
-    // on input.txt my anwser is too high (trying one lower also resulted in too high).
-    let grid = get_input();
+    let grid = get_input("input.txt");
 
-    let guard_position = find_positions(&grid, GUARD_START);
-    assert!(guard_position.len() == 1);
+    let guard_positions = find_positions(&grid, GUARD_START);
 
-    let obstacles_positions: Vec<(usize, usize)> = find_positions(&grid, OBSTABLE);
+    assert!(guard_positions.len() == 1);
+    let (guard_start_y, guard_start_x) = guard_positions[0];
+
+    let obstacles_positions: Vec<(usize, usize)> = find_positions(&grid, OBSTACLE);
     assert!(!obstacles_positions.is_empty());
 
     let mut guard = Guard {
-        x: guard_position[0].1,
-        y: guard_position[0].0,
+        x: guard_start_x,
+        y: guard_start_y,
         speed: 1,
-        // Start at one because the guards starting position counts as distinct.
-        distinct_positions: 1,
+        distinct_positions: Vec::new(),
         direction: Direction::Up,
         grid: &grid,
     };
+    for _ in &grid {
+        let row = vec![false; grid[0].len()];
+        guard.distinct_positions.push(row);
+    }
+    // Add starting position.
+    guard.distinct_positions[guard.y][guard.x] = true;
 
     let mut out_of_bounds = OutOfBounds::False;
+    let mut distinct_positions = 0;
 
-    let grid_size = grid.len() as f32;
-    let game_size = screen_width().min(screen_height());
-    let sq_size = game_size / grid_size;
-    let offset_x = sq_size;
-    let offset_y = sq_size;
-    clear_background(BLACK);
-    loop {
-        if is_key_down(KeyCode::Q) {
-            println!("Exitting...");
-            order_quit();
-        }
+    let visualize = std::env::args().nth(1).is_some();
+    if visualize {
+        let grid_size = grid.len() as f32;
+        let game_size = screen_width().min(screen_height());
+        let sq_size = game_size / grid_size;
+        let offset_x = sq_size;
+        let offset_y = sq_size;
 
-        for pos in obstacles_positions.iter() {
+        clear_background(BLACK);
+
+        loop {
+            if is_key_down(KeyCode::Q) {
+                println!("Exitting...");
+                order_quit();
+            }
+
+            if matches!(out_of_bounds, OutOfBounds::True) {
+                if distinct_positions == 0 {
+                    for row in &guard.distinct_positions {
+                        for col in row {
+                            if *col {
+                                distinct_positions += 1;
+                            }
+                        }
+                    }
+                }
+                let text = format!(
+                    "The guards visited a total amount of {} distinct positions.",
+                    distinct_positions
+                );
+                let font_size = 30.;
+                let text_size = measure_text(&text, None, font_size as _, 1.0);
+
+                draw_text(
+                    &text,
+                    screen_width() / 2. - text_size.width / 2.,
+                    screen_height() / 2. + text_size.height / 2.,
+                    font_size,
+                    WHITE,
+                );
+            } else {
+                out_of_bounds = guard.locomote();
+            }
+
+            for pos in obstacles_positions.iter() {
+                draw_rectangle(
+                    pos.1 as f32 * offset_x,
+                    pos.0 as f32 * offset_y,
+                    sq_size,
+                    sq_size,
+                    BROWN,
+                );
+            }
             draw_rectangle(
-                pos.1 as f32 * offset_x,
-                pos.0 as f32 * offset_y,
+                guard.x as f32 * offset_x,
+                guard.y as f32 * offset_y,
                 sq_size,
                 sq_size,
-                BROWN,
-            );
-        }
-        draw_rectangle(
-            guard.x as f32 * offset_x,
-            guard.y as f32 * offset_y,
-            sq_size,
-            sq_size,
-            WHITE,
-        );
-
-        if matches!(out_of_bounds, OutOfBounds::True) {
-            let text = format!(
-                "The guards visited a total amount of {} distinct positions.",
-                guard.distinct_positions
-            );
-            let font_size = 30.;
-            let text_size = measure_text(&text, None, font_size as _, 1.0);
-
-            draw_text(
-                &text,
-                screen_width() / 2. - text_size.width / 2.,
-                screen_height() / 2. + text_size.height / 2.,
-                font_size,
                 WHITE,
             );
-        } else {
+            print!("{}[2J", 27 as char);
+            for row in &guard.distinct_positions {
+                for col in row {
+                    if *col {
+                        print!("X");
+                    } else {
+                        print!(".");
+                    }
+                }
+                println!();
+            }
+            sleep(Duration::from_millis(500));
+            next_frame().await
+        }
+    } else {
+        while matches!(out_of_bounds, OutOfBounds::False) {
             out_of_bounds = guard.locomote();
         }
-
-        next_frame().await
+        for row in guard.distinct_positions {
+            for col in row {
+                if col {
+                    distinct_positions += 1;
+                }
+            }
+        }
+        println!(
+            "The guards visited a total amount of {} distinct positions.",
+            distinct_positions
+        );
     }
 }
 
@@ -112,13 +160,13 @@ enum Direction {
     Left,
 }
 
-struct Guard<'a> {
+struct Guard<'g> {
     x: usize,
     y: usize,
     speed: usize,
     direction: Direction,
-    distinct_positions: u32,
-    grid: &'a Grid,
+    distinct_positions: Vec<Vec<bool>>,
+    grid: &'g Grid,
 }
 
 enum OutOfBounds {
@@ -167,16 +215,16 @@ impl Guard<'_> {
         match self.grid[y][x] {
             '#' => self.direction = potential_new_direction,
             _ => {
-                self.distinct_positions += 1;
                 self.x = x;
                 self.y = y;
+                self.distinct_positions[self.y][self.x] = true;
             }
         }
     }
 }
 
-fn get_input() -> Grid {
-    let mut f = File::open("small.txt").unwrap();
+fn get_input(path: &str) -> Grid {
+    let mut f = File::open(path).unwrap();
     let mut input = String::new();
     f.read_to_string(&mut input).unwrap();
 
